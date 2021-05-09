365a94d5efd0c218ac0d5becf2fef63ceb91a0af
MustafaAli789 <alimustafa0987@gmail.com>
2021-05-09 16:03:41 -0400

    Display list of items

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero.interface';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,12 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = HEROES;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
-  hero: String = "windstorm";
 
 }
